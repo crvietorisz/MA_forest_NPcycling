f59e61361e7f0a8a7ce07e2e437c629250d6a1dd
crvietorisz <cviet@bu.edu>
2024-11-25 12:48:18 -0500

    Correlation script

---
+++ Statistical Analyses/Pearson correlations/WholeData_correlations.R
@@ -0,0 +1,15 @@
+# MA_forest_NPcycling 
+# Run Pearson correlations between all variables in our dataset
+# 11/25/24 C. Vietorisz
+
+setwd("MA_forest_NPcycling/")
+
+# read in full data
+sub_dist <- read.csv("Data/MA_forest_NPcycling_fullData.csv", row.names=1)
+# subset out just the continuous variables
+contin <- sub_dist[4:185]
+
+# run Pearson correlations
+library(Hmisc)
+cor_value <- rcorr(as.matrix(contin))$r # the correlation value
+p_value <- signif(rcorr(as.matrix(contin))$P,2) # the P value for each correlation
