03fabf522f189969fb9dc609073d29caddfa03d9
crvietorisz <cviet@bu.edu>
2024-12-02 14:52:46 -0500

    Update metadata

--- Statistical Analyses/Pearson correlations/WholeData_correlations.R
+++ Statistical Analyses/Pearson correlations/WholeData_correlations.R
@@ -7,7 +7,7 @@ setwd("MA_forest_NPcycling/")
 # read in full data
 sub_dist <- read.csv("Data/MA_forest_NPcycling_fullData.csv", row.names=1)
 # subset out just the continuous variables
-contin <- sub_dist[4:185]
+contin <- sub_dist[5:186]
 
 # run Pearson correlations
 library(Hmisc)
